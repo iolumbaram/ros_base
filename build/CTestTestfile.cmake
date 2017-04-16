b4f6d125ee9a41c70c03a3224e0207e4f60eaec9
BluePrata <hazel77744@gmail.com>
2017-04-16 17:46:20 +0800

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/emt_tecksang/ros_base/src
+# Build directory: /home/emt_tecksang/ros_base/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/emt_tecksang/ros_base/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/emt_tecksang/ros_base/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/emt_tecksang/ros_base/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/emt_tecksang/ros_base/src:$ROS_PACKAGE_PATH"
